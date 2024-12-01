752d7919ed403ed7795a91181fb229c481624ba7
Hernandes Andrade <hernandesandrade12@gmail.com>
2024-12-01 16:52:54 -0300

    Criei a pagina show
    Também foi ajustado algumas chamadas de link usando thymeleaf, como o detalhes e o submit agora é redirecionado para a pagina show

--- src/main/java/com/xavecoding/regescweb/controllers/ProfessorController.java
+++ src/main/java/com/xavecoding/regescweb/controllers/ProfessorController.java
@@ -4,16 +4,19 @@ import com.xavecoding.regescweb.dto.RequisicaoNovoProfessor;
 import com.xavecoding.regescweb.models.Professor;
 import com.xavecoding.regescweb.models.StatusProfessor;
 import com.xavecoding.regescweb.repositories.ProfessorRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ProfessorController {
@@ -46,7 +49,22 @@ public class ProfessorController {
         }else{
             Professor professor = requisicao.toProfessor();
             professorRepository.save(professor);
-            return new ModelAndView("redirect:professores");
+            return new ModelAndView("redirect:professores/" + professor.getId());
+        }
+    }
+
+    @GetMapping("/professores/{id}")
+    public ModelAndView show(@PathVariable Long id){
+        Optional<Professor> optional = this.professorRepository.findById(id);
+        if (optional.isPresent()){
+            Professor professor = optional.get();
+            ModelAndView mv = new ModelAndView("professores/show");
+            mv.addObject("professor", professor);
+            return mv;
+
+        }else{
+
+            return new ModelAndView("redirect:/professores");
         }
     }
 
